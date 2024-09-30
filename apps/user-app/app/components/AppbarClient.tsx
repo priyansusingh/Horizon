aa487316252f003fbb7da24cd413e686d2a8cb32
Priyansu Singh <priyansusingh3344@gmail.com>
2024-10-01 00:58:21 +0530

    fe changes

---
+++ apps/user-app/app/components/AppbarClient.tsx
@@ -0,0 +1,16 @@
+import {Appbar} from '@repo/ui/Appbar'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+
+
+export const AppbarClient = ()=>{
+   const router = useRouter();
+   const session = useSession();
+    return <div>
+        <Appbar onSignin={signIn} onSignout={async ()=>{
+            await signOut()
+            router.push("/api/auth/signin")
+        }} user={session.data?.user}/>
+    </div>
+}
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,9 @@
-"use client"
-import { signIn, signOut, useSession } from "next-auth/react";
-import {Appbar} from "@repo/ui/Appbar"
 
 export default function Home() {
-  const session = useSession();
+
   return (
     <div>
-      <Appbar   onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+     hii there
     </div>
   );
 }
