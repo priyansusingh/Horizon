60155c17f0e3339603f26542fcd95eac4cb44b59
Priyansu Singh <priyansusingh3344@gmail.com>
2024-09-30 19:46:03 +0530

    sigin page

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,13 @@
-import CountRenderer from "./components/CountRendered";
+"use client"
 
+import { signIn, signOut } from "next-auth/react";
+import CountRenderer from "./components/CountRendered";
+import {Appbar} from "@repo/ui/Appbar"
 export default function Home() {
  
   return (
     <div>
+      <Appbar onSignin={signIn} onSignout={signOut}/>
     <CountRenderer/>
     </div>
   );
