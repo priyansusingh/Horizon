9931245f6096f9af3227cf8759615845fc2d93db
Priyansu Singh <priyansusingh3344@gmail.com>
2024-10-01 19:38:46 +0530

    fe

--- apps/user-app/app/components/AppbarClient.tsx
+++ apps/user-app/app/components/AppbarClient.tsx
@@ -1,6 +1,7 @@
+"use client"
 import {Appbar} from '@repo/ui/Appbar'
 import { signIn, signOut, useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 
 
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,13 @@
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "./lib/auth";
 
-export default function Home() {
-
-  return (
-    <div>
-     hii there
-    </div>
-  );
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
+  
 }
